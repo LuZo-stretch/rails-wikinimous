90f834460a3012aa883120656f7195379a6d0aa3
Lucia Galarza <lzgalarza@gmail.com>
2024-02-01 17:38:54 +0000

    Wikinimous halfway - routes set, needs coding show, edit, delete and styles

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "articles", to: "articles#index"
+  get "articles/new", to: "articles#new"
+  post "articles", to: "articles#create"
+  get "articles/:id", to: "articles#show", as: :article
+  get "articles/:id/edit", to: "articles#edit"
 end
